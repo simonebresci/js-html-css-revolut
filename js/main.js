bb549fb53a80df05c841de10b86c2451558f8dd2
Simone <simone.bresci.05@gmail.com>
2021-01-14 18:20:06 +0100

    struttura dropdown menu + toggle

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@
 // Dobbiamo copiare la nav da questo sito https://www.revolut.com/en-IT/ e sviluppare il suo dropdown.
 // Mi raccomando: solo la prima parte, solo la navbar.
 // In questo caso quindi lavoreremo anche lato HTML. Sviluppiamo il nostro html + css e il nostro codice jquery per far comparire o sparire il dropdown menu.
-alert('revolut');
+
+
+
+$(document).ready(function(){
+
+
+  $('.navbar-main .menu .link').click(function(){
+    // Chiudi tutti i dropdown
+    $('.navbar-main .menu .link .dropdown-box').css('display','none');
+
+    // Apri/chiudi menu dropdown quand link viene cliccato
+    $(this).find('.dropdown-box').fadeToggle();
+  })
+
+
+});
