4966228fe05bce28d9e27ce8217925ec6db98682
Simone <simone.bresci.05@gmail.com>
2021-01-14 16:46:02 +0100

    inizio progetto

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Descrizione:
+// Sviluppiamo il fratello maggiore del dropdown visto oggi in classe.
+// Dobbiamo copiare la nav da questo sito https://www.revolut.com/en-IT/ e sviluppare il suo dropdown.
+// Mi raccomando: solo la prima parte, solo la navbar.
+// In questo caso quindi lavoreremo anche lato HTML. Sviluppiamo il nostro html + css e il nostro codice jquery per far comparire o sparire il dropdown menu.
+alert('revolut');
